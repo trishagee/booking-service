fe1f2c004cfadfcadf9ab1859170e0acbc12a8e3
Trisha Gee <trisha.gee@gmail.com>
2021-04-23 16:11:23 +0200

    Initial commit. Spring Boot booking service with Gradle, Java 16, Spring Data JPA, and Spring MVC

---
+++ src/main/java/com/jetbrains/bookingservice/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.bookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jetbrains/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
