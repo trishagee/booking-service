0247818d49b2039c50310a5417302b43821fb1eb
Trisha Gee <trisha.gee@gmail.com>
2021-04-29 16:06:43 +0200

    Additional integration tests to prove the business logic works the way it's supposed to.

--- src/main/java/com/jetbrains/bookingservice/NoAvailableCapacityException.java
+++ src/main/java/com/jetbrains/bookingservice/NoAvailableCapacityException.java
@@ -1,5 +1,9 @@
 package com.jetbrains.bookingservice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "NoAvailableCapacityException")
 public class NoAvailableCapacityException extends BookingException {
     public NoAvailableCapacityException(final String message) {
         super(message);
--- src/test/java/com/jetbrains/bookingservice/BookingServiceIntegrationTest.java
+++ src/test/java/com/jetbrains/bookingservice/BookingServiceIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.jetbrains.bookingservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,7 +33,7 @@ public class BookingServiceIntegrationTest {
     @DisplayName("Should be able to create a simple booking")
     void shouldBeAbleToCreateASimpleBooking() throws Exception {
         bookingRepository.deleteAll();
-        //client selects a restaurant with an id and passes the id, date and time, number of diners, indoor vs outdoor
+        //client selects a restaurant with an id and passes the id, date and time, number of diners
         Booking booking = new Booking("1", LocalDate.of(2021, 4, 26), 4);
 
         // when
@@ -57,10 +57,9 @@ public class BookingServiceIntegrationTest {
 
     @Test
     @DisplayName("Should not allow a booking on a day where existing bookings take up the capacity")
-    @Disabled("not finished figuring out the correct response")
     void shouldNotAllowABookingOnADayWhereExistingBookingsTakeUpTheCapacity() throws Exception {
         // given:
-        // three existing bookings in the database, 2 to nearly fill Dalia's restaurant
+        // existing bookings in the database, 2 to nearly fill Dalia's restaurant
         Booking booking1 = new Booking("1", LocalDate.of(2021, 4, 26), 11);
         bookingRepository.save(booking1);
         Booking booking2 = new Booking("1", LocalDate.of(2021, 4, 26), 7);
@@ -68,21 +67,71 @@ public class BookingServiceIntegrationTest {
 
         // when
         Booking newBooking = new Booking("1", LocalDate.of(2021, 4, 26), 4);
-        MvcResult mvcResult = mockMvc.perform(post("/bookings")
-                                                      .contentType("application/json")
-                                                      .content(objectMapper.writeValueAsString(newBooking)))
-                                     .andExpect(status().isOk())
-                                     .andReturn();
-        // currently throwing NoAvailableCapacityException, not sure what behaviour is expected at the http level
-
-        fail("Not implemented");
+        mockMvc.perform(post("/bookings")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(newBooking)))
+               .andExpect(status().isConflict())
+               .andExpect(status().reason("NoAvailableCapacityException"));
     }
 
-    // test - other restaurnat's capacities don't impact this one
-    //
+    @Test
+    void shouldBeAbleToCreateABookingEvenWhenThereAreOtherBookingsOnADifferentDay() throws Exception {
+        bookingRepository.deleteAll();
+        // existing bookings in the database, 2 to fill Helen's restaurant on a different day
+        Booking booking1 = new Booking("2", LocalDate.of(2021, 4, 25), 11);
+        bookingRepository.save(booking1);
+        Booking booking2 = new Booking("2", LocalDate.of(2021, 4, 25), 19);
+        bookingRepository.save(booking2);
 
-    // check restaurant capacity
-    //  -  on that date
-    //  -  for that capacity
-    //  -  indoors vs outdoors
+        Booking booking = new Booking("2", LocalDate.of(2021, 4, 24), 4);
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(post("/bookings")
+                                                      .contentType("application/json")
+                                                      .content(objectMapper.writeValueAsString(booking)))
+                                     .andExpect(status().isOk())
+                                     .andReturn();
+        Booking returnedBooking = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Booking.class);
+
+        // then
+        Optional<Booking> bookingById = bookingRepository.findById(returnedBooking.getId());
+        assertTrue(bookingById.isPresent());
+
+        Booking actualBooking = bookingById.get();
+        // consider moving this into a helper method
+        assertEquals(booking.getRestaurantId(), actualBooking.getRestaurantId());
+        assertEquals(booking.getDate(), actualBooking.getDate());
+        assertEquals(booking.getNumberOfDiners(), actualBooking.getNumberOfDiners());
+    }
+
+    @Test
+    void shouldBeAbleToCreateABookingEvenWhenThereAreOtherBookingsAtADifferentRestaurant() throws Exception {
+        bookingRepository.deleteAll();
+        // existing bookings in the database, 2 to fill Trisha's restaurant
+        Booking booking1 = new Booking("3", LocalDate.of(2021, 4, 27), 11);
+        bookingRepository.save(booking1);
+        Booking booking2 = new Booking("3", LocalDate.of(2021, 4, 27), 1);
+        bookingRepository.save(booking2);
+
+        Booking booking = new Booking("4", LocalDate.of(2021, 4, 27), 15);
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(post("/bookings")
+                                                      .contentType("application/json")
+                                                      .content(objectMapper.writeValueAsString(booking)))
+                                     .andExpect(status().isOk())
+                                     .andReturn();
+        Booking returnedBooking = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Booking.class);
+
+        // then
+        Optional<Booking> bookingById = bookingRepository.findById(returnedBooking.getId());
+        assertTrue(bookingById.isPresent());
+
+        Booking actualBooking = bookingById.get();
+        // consider moving this into a helper method
+        assertEquals(booking.getRestaurantId(), actualBooking.getRestaurantId());
+        assertEquals(booking.getDate(), actualBooking.getDate());
+        assertEquals(booking.getNumberOfDiners(), actualBooking.getNumberOfDiners());
+    }
+
 }
