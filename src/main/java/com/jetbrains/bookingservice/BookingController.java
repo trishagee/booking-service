f8a643baf4c9b0df468448b2a9cd7df6e97c7bb8
Trisha Gee <trisha.gee@gmail.com>
2021-05-04 16:38:28 +0200

    First attempt to increase readability of createBooking - adding comments

--- src/main/java/com/jetbrains/bookingservice/BookingController.java
+++ src/main/java/com/jetbrains/bookingservice/BookingController.java
@@ -28,18 +28,22 @@ public class BookingController {
         // In a "real" environment this would at the very least be a property/environment variable, but ideally something like Service Discovery like Eureka
         Restaurant restaurant = restTemplate.getForObject("http://localhost:8080/restaurants/" + booking.getRestaurantId(), Restaurant.class);
 
+        // check if the restaurant actually exists 
         if (restaurant == null) {
             throw new RestaurantNotFoundException(booking.getRestaurantId());
         }
 
+        // check if the number of diners in the booking is more than the number of seats in the restaurant
         if (restaurant.capacity() < booking.getNumberOfDiners()) {
             throw new NoAvailableCapacityException("Number of diners exceeds available restaurant capacity");
         }
 
+        // check the restaurant is open on the day of the booking
         if (!restaurant.openingDays().contains(booking.getDate().getDayOfWeek())) {
             throw new RestaurantClosedException("Restaurant is not open on: " + booking.getDate());
         }
 
+        // find all the bookings for that day and check that with all the booked diners the restaurant still has space for the new booking diners
         List<Booking> allByRestaurantIdAndDate = repository.findAllByRestaurantIdAndDate(booking.getRestaurantId(), booking.getDate());
         int totalDinersOnThisDay = allByRestaurantIdAndDate
                                              .stream().mapToInt(Booking::getNumberOfDiners).sum();
@@ -47,6 +51,7 @@ public class BookingController {
             throw new NoAvailableCapacityException("Restaurant all booked up!");
         }
 
+        // if we got this far, the booking is valid and we can save it
         return repository.save(booking);
     }
 
