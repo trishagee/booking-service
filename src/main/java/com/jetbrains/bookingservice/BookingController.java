419107f7da52bc33a9141366a690861db4d627c3
Trisha Gee <trisha.gee@gmail.com>
2021-05-04 16:43:22 +0200

    Replacing all validation sections of createBooking with private methods, and removing comments.

--- src/main/java/com/jetbrains/bookingservice/BookingController.java
+++ src/main/java/com/jetbrains/bookingservice/BookingController.java
@@ -29,25 +29,10 @@ public class BookingController {
         // In a "real" environment this would at the very least be a property/environment variable, but ideally something like Service Discovery like Eureka
         Restaurant restaurant = restTemplate.getForObject("http://localhost:8080/restaurants/" + booking.getRestaurantId(), Restaurant.class);
 
-        // check if the restaurant actually exists 
         throwExceptionIfRestaurantNotValid(restaurant, booking.getRestaurantId());
-
-        // check if the number of diners in the booking is more than the number of seats in the restaurant
-        if (restaurant.capacity() < booking.getNumberOfDiners()) {
-            throw new NoAvailableCapacityException("Number of diners exceeds available restaurant capacity");
-        }
-
-        // check the restaurant is open on the day of the booking
-        if (!restaurant.openingDays().contains(booking.getDate().getDayOfWeek())) {
-            throw new RestaurantClosedException("Restaurant is not open on: " + booking.getDate());
-        }
-
-        // find all the bookings for that day and check that with all the booked diners the restaurant still has space for the new booking diners
-        List<Booking> allByRestaurantIdAndDate = repository.findAllByRestaurantIdAndDate(booking.getRestaurantId(), booking.getDate());
-        int totalDinersOnThisDay = allByRestaurantIdAndDate.stream().mapToInt(Booking::getNumberOfDiners).sum();
-        if (totalDinersOnThisDay + booking.getNumberOfDiners() > restaurant.capacity()) {
-            throw new NoAvailableCapacityException("Restaurant all booked up!");
-        }
+        throwExceptionIfBookingHasMoreDinersThanRestaurantHasSeats(booking, restaurant);
+        throwExceptionIfRestaurantIsClosedOnBookingDate(booking, restaurant);
+        throwExceptionIfRestaurantDoesNotHaveEnoughSpaceOnThatDate(booking, restaurant);
 
         // if we got this far, the booking is valid and we can save it
         return repository.save(booking);
@@ -56,6 +41,27 @@ public class BookingController {
     private void throwExceptionIfRestaurantNotValid(@Nullable Restaurant restaurant, String restaurantId) {
         if (restaurant == null) {
             throw new RestaurantNotFoundException(restaurantId);
+        }
+    }
+
+    private void throwExceptionIfBookingHasMoreDinersThanRestaurantHasSeats(Booking booking, Restaurant restaurant) {
+        if (restaurant.capacity() < booking.getNumberOfDiners()) {
+            throw new NoAvailableCapacityException("Number of diners exceeds available restaurant capacity");
+        }
+    }
+
+    private void throwExceptionIfRestaurantIsClosedOnBookingDate(Booking booking, Restaurant restaurant) {
+        if (!restaurant.openingDays().contains(booking.getDate().getDayOfWeek())) {
+            throw new RestaurantClosedException("Restaurant is not open on: " + booking.getDate());
+        }
+    }
+
+    private void throwExceptionIfRestaurantDoesNotHaveEnoughSpaceOnThatDate(Booking booking, Restaurant restaurant) {
+        // find all the bookings for that day and check that with all the booked diners the restaurant still has space for the new booking diners
+        List<Booking> allByRestaurantIdAndDate = repository.findAllByRestaurantIdAndDate(booking.getRestaurantId(), booking.getDate());
+        int totalDinersOnThisDay = allByRestaurantIdAndDate.stream().mapToInt(Booking::getNumberOfDiners).sum();
+        if (totalDinersOnThisDay + booking.getNumberOfDiners() > restaurant.capacity()) {
+            throw new NoAvailableCapacityException("Restaurant all booked up!");
         }
     }
 
