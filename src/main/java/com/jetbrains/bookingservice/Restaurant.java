ba120e81c76d417383be9ed1cfc54a8c76fb5610
Pulkit Gupta <pulkitent@gmail.com>
2021-09-18 04:09:33 +0530

    Add Restaurant.java & it's unit test

--- src/main/java/com/jetbrains/bookingservice/Restaurant.java
+++ src/main/java/com/jetbrains/bookingservice/Restaurant.java
@@ -3,6 +3,17 @@ package com.jetbrains.bookingservice;
 import java.time.DayOfWeek;
 import java.util.Set;
 
-public record Restaurant(String id, int capacity, Set<DayOfWeek> openingDays) {
+public record Restaurant(String id, Integer capacity, Set<DayOfWeek> openingDays) {
 
+    Boolean isCapacityLessThanNumberOfDinersIn(final Booking booking) {
+      return booking.isNumberOfDinersMoreThanGivenCapacity(this.capacity);
+    }
+
+    Boolean isRestaurantOpenOn(final DayOfWeek dayOfWeek) {
+      return this.openingDays.contains(dayOfWeek);
+    }
+
+    Boolean isBookingPossibleForGiven(final Booking booking, final Integer totalDinersOnThisDay) {
+      return (booking.getSumOfBookingDinersAnd(totalDinersOnThisDay)) <= this.capacity;
+    }
 }
---
+++ src/test/java/com/jetbrains/bookingservice/RestaurantTest.java
@@ -0,0 +1,112 @@
+package com.jetbrains.bookingservice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
+import static java.time.DayOfWeek.THURSDAY;
+import static java.time.DayOfWeek.TUESDAY;
+import static java.time.DayOfWeek.WEDNESDAY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class RestaurantTest {
+    @Test
+    @DisplayName("Test isCapacityLessThanNumberOfDinersIn should return false when number of diners does not exceed capacity")
+    void testIsCapacityLessThanNumberOfDinersIn_shouldReturnFalseWhenCapacityNotExceedsNumberOfDiners() {
+    // Arrange
+      String restaurantId = "2";
+      int capacity = 10;
+      Set<DayOfWeek> dayOfWeeks = Set.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY);
+      Restaurant restaurant = new Restaurant(restaurantId, capacity, dayOfWeeks);
+      LocalDate now = LocalDate.now();
+      int numberOfDiners = 2;
+      Booking booking = new Booking(restaurantId, now, numberOfDiners);
+
+    // Action & Assert
+    assertEquals(false, restaurant.isCapacityLessThanNumberOfDinersIn(booking));
+  }
+
+    @Test
+    @DisplayName("Test isCapacityLessThanNumberOfDinersIn should return true when number of diners exceeds capacity")
+    void testIsCapacityLessThanNumberOfDinersIn_shouldReturnTrueWhenCapacityExceedsNumberOfDiners() {
+      // Arrange
+      String restaurantId = "2";
+      int capacity = 1;
+      Set<DayOfWeek> dayOfWeeks = Set.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY);
+      Restaurant restaurant = new Restaurant(restaurantId, capacity, dayOfWeeks);
+      LocalDate now = LocalDate.now();
+      int numberOfDiners = 20;
+      Booking booking = new Booking(restaurantId, now, numberOfDiners);
+
+      // Action & Assert
+      assertEquals(true, restaurant.isCapacityLessThanNumberOfDinersIn(booking));
+    }
+
+    @Test
+    @DisplayName("Test isRestaurantOpenOn should return true when restaurant is open all days of week")
+    void testIsRestaurantOpenOn_shouldReturnTrueWhenRestaurantIsOpen() {
+      // Arrange
+      String restaurantId = "2";
+      int capacity = 10;
+      Set<DayOfWeek> dayOfWeeks = Set.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY);
+      Restaurant restaurant = new Restaurant(restaurantId, capacity, dayOfWeeks);
+
+      // Action & Assert
+      assertEquals(true, restaurant.isRestaurantOpenOn(MONDAY));
+    }
+
+    @Test
+    @DisplayName("Test isRestaurantOpenOn should return false when restaurant is closed on monday")
+    void testIsRestaurantOpenOn_shouldReturnFalseWhenRestaurantIsClosed() {
+      // Arrange
+      String restaurantId = "2";
+      int capacity = 10;
+      Set<DayOfWeek> dayOfWeeks = Set.of(TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY);
+      Restaurant restaurant = new Restaurant(restaurantId, capacity, dayOfWeeks);
+
+      // Action & Assert
+      assertEquals(false, restaurant.isRestaurantOpenOn(MONDAY));
+    }
+
+    @Test
+    @DisplayName("Test isBookingPossibleForGiven should return true when booking is possible")
+    void testIsBookingPossibleForGiven_shouldReturnTrue() {
+      // Arrange
+      String restaurantId = "2";
+      int capacity = 10;
+      Set<DayOfWeek> dayOfWeeks = Set.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY);
+      Restaurant restaurant = new Restaurant(restaurantId, capacity, dayOfWeeks);
+      LocalDate now = LocalDate.now();
+      int numberOfDiners = 2;
+      Booking booking = new Booking(restaurantId, now, numberOfDiners);
+
+      // Action & Assert
+      assertEquals(true, restaurant.isBookingPossibleForGiven(booking, 8));
+    }
+
+    @Test
+    @DisplayName("Test isBookingPossibleForGiven should return false when booking is not possible")
+    void testIsBookingPossibleForGiven_shouldReturnFalse() {
+      // Arrange
+      String restaurantId = "2";
+      int capacity = 10;
+      Set<DayOfWeek> dayOfWeeks = Set.of(TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY);
+      Restaurant restaurant = new Restaurant(restaurantId, capacity, dayOfWeeks);
+      LocalDate now = LocalDate.now();
+      int numberOfDiners = 2;
+      Booking booking = new Booking(restaurantId, now, numberOfDiners);
+
+      // Action & Assert
+      assertEquals(false, restaurant.isBookingPossibleForGiven(booking, 10));
+    }
+}
