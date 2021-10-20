850211a2e3e4e0a8bcb52362f78ead3995567cc9
Maarten Clymer <maarten@maartenballiauw.be>
2021-10-20 09:18:20 +0000

    Update container URL

--- .space.kts
+++ .space.kts
@@ -33,7 +33,7 @@ job("Build and publish BookingService container") {
             labels["vendor"] = "JetBrains"
         }
 
-        push("registry.jetbrains.team/p/blossom/containers/booking-service") {
+        push("demo.registry.jetbrains.space/p/blossom/containers/booking-service") {
             tags(containerTagVersion)
         }
     }
