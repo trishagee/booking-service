de2bd46b6c897645544796d4a23081d9df19ac54
Trisha <trisha.gee@gmail.com>
2023-04-13 19:18:50 +0200

    Upgrading Spring Boot and MySql. Includes moving to jakarta package name.

--- src/main/java/com/jetbrains/bookingservice/Booking.java
+++ src/main/java/com/jetbrains/bookingservice/Booking.java
@@ -1,8 +1,9 @@
 package com.jetbrains.bookingservice;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
 
 @Entity
